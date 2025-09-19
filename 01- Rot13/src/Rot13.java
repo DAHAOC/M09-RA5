dc9546d0a072742437ec1561ca85b06454886ca6
Dahao <2024_dahao.chen@iticbcn.cat>
2025-09-19 17:29:16 +0200

    Acabado el ejercicio, se añadio las dos funciones.

---
+++ 01- Rot13/src/Rot13.java
@@ -0,0 +1,63 @@
+
+public class Rot13 {
+    public static void main(String[] args) {
+        System.out.println("Text a codificar");
+        String text = "Hola, texto para probar";
+        String codificat = xifraRot13(text);
+        System.out.println(codificat);
+        System.out.println(desxifraRot13(codificat));
+    }
+    public static String xifraRot13(String input) {
+        char[] abcminus = "abcdefghijklmnñopqrstuvwxyzáàéèíìïóòúùüç".toCharArray();
+        char[] abcmayus = "ABCDEFGHIJKLMNÑIOQRSTUVWXYZÁÀÉÈÍÌÏÓÒÚÙÜÇ".toCharArray();
+        String retornar = "";
+        for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(Character.isWhitespace(c) || !Character.isLetter(c)) {
+                retornar += c;
+                continue;
+            }
+
+            for(int e = 0; e < abcminus.length; e++) {
+                char c2 = abcminus[e];
+                if (Character.toLowerCase(c) == c2) {
+                    if(Character.isUpperCase(c)) {
+                        retornar += abcmayus[(e + 13) % 40];
+                    } else {
+                        retornar += abcminus[(e + 13) % 40];
+                    }
+                } else {
+                    continue;
+                }
+            } 
+          
+        }
+        return retornar;
+    }
+    
+    public static String desxifraRot13(String input) {
+        char[] abcminus = "abcdefghijklmnñopqrstuvwxyzáàéèíìïóòúùüç".toCharArray();
+        char[] abcmayus = "ABCDEFGHIJKLMNÑIOQRSTUVWXYZÁÀÉÈÍÌÏÓÒÚÙÜÇ".toCharArray();
+        String retornar ="";
+
+        for(int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+            if(Character.isWhitespace(c) || !Character.isLetter(c)) {
+                retornar += c;
+                continue;
+            }
+            
+            for(int e = 0; e < abcminus.length; e++) {
+                char c2 = abcminus[e];
+                if(Character.toLowerCase(c) == c2) {
+                    if(Character.isUpperCase(c)) {
+                        retornar += abcmayus[(e-13) % 40];
+                    } else {
+                        retornar += abcminus[(e-13) % 40];
+                    }
+                } 
+            }
+        }
+        return retornar;
+    }
+} 
